36729cb6decbef1b0540439692464cae764ba9bb
github username <suoarik@gmail.com>
2018-12-08 20:35:09 +0000

    bones of a react app to reack blood borne progress

---
+++ src/containers/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return ( 
+            <div>
+                <div class="ui mini menu">
+                    <a class="active item">
+                        Home
+                    </a>
+                    <a class="item">
+                        Messages
+                    </a>
+                    <div class="right menu">
+                        <div class="ui dropdown item">
+                            Language <i class="dropdown icon"></i>
+                            <div class="menu">
+                                <a class="item">English</a>
+                                <a class="item">Russian</a>
+                                <a class="item">Spanish</a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="ui primary button">Sign Up</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+         );
+    }
+}
+ 
+export default Navbar;
